719b277aa8ec3a21db014ac38b01afeed9dfbf63
guillain bisimwa <guillainbisimwa@hotmail.fr>
2019-12-26 08:46:29 -0500

    create a linkSchema

---
+++ src/schemas/index.js
@@ -0,0 +1,14 @@
+import clientSchema from './client';
+import groupSchema from './group';
+import { gql } from 'apollo-server';
+
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+`;
+
+export default [linkSchema, clientSchema, groupSchema];
